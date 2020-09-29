80f551b4979c9cec74dcb149580a55ef114def4d
Ok Jae Ook <okjaeook98@gmail.com>
2020-09-29 13:32:42 +0900

    30Cm 전진, 180도 회전, 30Cm 전진

---
+++ 5주차/User_Function_Homework2/User_Function_Homework2.ino
@@ -0,0 +1,29 @@
+#include <Servo.h> // Include servo library
+Servo servoLeft; // Declare left and right servos
+Servo servoRight;
+void setup() // Built-in initialization block
+{
+  tone(4, 3000, 1000); // Play tone for 1 second
+  delay(1000); // Delay to finish tone
+  servoLeft.attach(13); // Attach left signal to pin 13
+  servoRight.attach(12); // Attach right signal to pin 12
+  maneuver(200, 200, 1700); // Forward 2 seconds
+  maneuver(200, -200, 995); // Right 1 seconds
+  maneuver(200, 200, 1700); // Forward 2 seconds
+  maneuver(0, 0, -1); // Disable servos
+}
+void loop() // Main loop auto-repeats
+{ // Empty, nothing needs repeating
+}
+void maneuver(int speedLeft, int speedRight, int msTime)
+{ // speedLeft, speedRight ranges: Backward Linear Stop Linear Forward
+  // -200 -100......0......100 200
+  servoLeft.writeMicroseconds(1500 + speedLeft); // Set Left servo speed
+  servoRight.writeMicroseconds(1500 - speedRight); //Set right servo speed
+  if (msTime == -1) // if msTime = -1
+  {
+    servoLeft.detach(); // Stop servo signals
+    servoRight.detach();
+  }
+  delay(msTime); // Delay for msTime
+}
