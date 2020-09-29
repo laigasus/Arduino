65385bdd96f8b19194f62c39597e1792e2fe9513
Ok Jae Ook <okjaeook98@gmail.com>
2020-09-29 13:32:51 +0900

    30Cm 전진, 90도 우회전 반복하여 제자리로 돌아오기

---
+++ 5주차/User_Function_Homework3/User_Function_Homework3.ino
@@ -0,0 +1,30 @@
+#include <Servo.h> // Include servo library
+Servo servoLeft; // Declare left and right servos
+Servo servoRight;
+void setup() // Built-in initialization block
+{
+  tone(4, 3000, 1000); // Play tone for 1 second
+  delay(1000); // Delay to finish tone
+  servoLeft.attach(13); // Attach left signal to pin 13
+  servoRight.attach(12); // Attach right signal to pin 12
+  for (int i = 1; i <= 4; i++) {
+    maneuver(200, 200, 2000); // Forward 2 seconds
+    maneuver(200, -200, 480); // Right 0.6   seconds
+  }
+  maneuver(0, 0, -1); // Disable servos
+}
+void loop() // Main loop auto-repeats
+{ // Empty, nothing needs repeating
+}
+void maneuver(int speedLeft, int speedRight, int msTime)
+{ // speedLeft, speedRight ranges: Backward Linear Stop Linear Forward
+  // -200 -100......0......100 200
+  servoLeft.writeMicroseconds(1500 + speedLeft); // Set Left servo speed
+  servoRight.writeMicroseconds(1500 - speedRight); //Set right servo speed
+  if (msTime == -1) // if msTime = -1
+  {
+    servoLeft.detach(); // Stop servo signals
+    servoRight.detach();
+  }
+  delay(msTime); // Delay for msTime
+}
