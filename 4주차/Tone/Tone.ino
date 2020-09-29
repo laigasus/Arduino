46f74e8e6408593250b3c35bfca1cd00d783260f
Ok Jae Ook <okjaeook98@gmail.com>
2020-09-29 13:30:23 +0900

    부저

---
+++ 4주차/Tone/Tone.ino
@@ -0,0 +1,14 @@
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Beep!!!");
+  tone(4, 3000, 1000);
+  delay(1000);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Waiting for reset...");
+  delay(1000);
+}
