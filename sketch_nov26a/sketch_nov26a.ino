c35968ab02af405e6919f8e860990059c0228a35
Ok Jae Ook <okjaeook98@gmail.com>
2020-11-27 13:25:20 +0900

    .

---
+++ sketch_nov26a/sketch_nov26a.ino
@@ -0,0 +1,19 @@
+// the setup function runs once when you press reset or power the board
+
+void setup() {
+
+    // initialize digital pin 13 as an output.
+
+    pinMode(5, OUTPUT);// Set A5 as OUTPUT
+
+}
+
+
+
+// the loop function runs over and over again forever
+
+void loop() {
+  /* code */
+  digitalWrite(5, HIGH);   // atomize
+  
+} 
